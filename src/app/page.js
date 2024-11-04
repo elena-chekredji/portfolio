9eb8909abd0c63ed2bfdf19519e7d6fb789a8ae0
Elena Chekredji <elena.chekredji@gmail.com>
2024-11-04 16:49:16 -0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
+
+export default function Home() {
+  return (
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <div class="container  mx-auto py-4 px-12">
+          <HeroSection />
+        </div>
+      </main>
+
+  );
+}
