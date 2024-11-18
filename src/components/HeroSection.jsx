784c7893687d85b66d5e09fe6e921e947b9876de
Elena Chekredji <elena.chekredji@gmail.com>
2024-11-18 09:38:21 -0500

    Made the text animated and fixed react version

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,20 +1,50 @@
+"use client";
 import React from 'react';
 import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+
+
+
 
 const HeroSection = () => {
     return <section>
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                    <span className="text-transparent">Hi, I am{" "}</span>
-                    Elena!
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                        Hi, I am{" "}
+                    </span>
+                    <br></br>
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            'Elena!',
+                            1000, // wait 1s before replacing "Mice" with "Hamsters"
+                            'a Frontend Developer!',
+                            1000,
+                            'a Software Engineer!',
+                            1000,
+                            'a UI/UX Designer!',
+                            1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        style={{ }}
+                        repeat={Infinity}
+                        />
                 </h1>
                 <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
                     I'm a web developer who loves to code and is very passionate about creating beautiful and user-friendly websites.
                 </p>
                 <div>
-                    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">Hire Me</button>
-                    <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-bg-slate=800 text-white border boarder-white mt-3">Download CV</button>
+                    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+                        Hire Me
+                    </button>
+                    <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-pink-500 hover:bg-bg-slate=800 text-white mt-3">
+                        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                            Download CV
+                        </span>
+                    </button>
                 </div>
             </div>
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
